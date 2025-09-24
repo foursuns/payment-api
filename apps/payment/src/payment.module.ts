70e40e0d693a04f13dfbe8de49f04705bff377d2
Marcio Anthony <anthony.foursuns@gmail.com>
2025-09-24 13:44:13 -0300

    refactor: changed payment module

--- apps/payment/src/payment.module.ts
+++ apps/payment/src/payment.module.ts
@@ -1,10 +1,48 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware, PrismaModule } from '@app/common';
+import helmet from 'helmet';
+import * as Joi from 'joi';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_NAME: Joi.string().required(),
+        API_PREFIX: Joi.string().required(),
+        APP_PAYMENT: Joi.string().required(),
+        CORS_ENABLED: Joi.string().required(),
+        CORS_ALLOWED_ORIGINS: Joi.string().required(),
+        CORS_ALLOWED_HEADERS: Joi.string().required(),
+        CORS_ALLOWED_METHODS: Joi.string().required(),
+        CORS_MAX_AGE: Joi.string().required(),
+        CORS_CREDENTIALS: Joi.string().required(),
+        PORT: Joi.number().default(5000),
+        SWAGGER_ENABLED: Joi.string().required(),
+        SWAGGER_PREFIX: Joi.string().required(),
+        SWAGGER_TITLE: Joi.string().required(),
+        SWAGGER_VERSION: Joi.string().required(),
+        SWAGGER_DESCRIPTION: Joi.string().required(),
+        SWAGGER_ENDPOINT: Joi.string().required(),
+        NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+        DATABASE_URL: Joi.string().required(),
+      }),
+      envFilePath: './apps/payment/.env',
+    }),
+    PrismaModule,
+  ],
+  exports: [],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
-export class PaymentModule {}
+export class PaymentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, helmet())
+      .exclude({ path: 'health', method: RequestMethod.ALL })
+      .forRoutes('*');
+  }
+}
